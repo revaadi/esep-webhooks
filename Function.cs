3522a1d54fb26a76a79f65b5d53e52a0c9664a2e
revaadi <reva.adi@ufl.edu>
2024-11-10 18:00:23 -0500

    Deployed Lambda function for GitHub webhook

---
+++ Function.cs
@@ -0,0 +1,30 @@
+using System.Text;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+    public string FunctionHandler(object input, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"FunctionHandler received: {input}");
+
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+   
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
+    }
+}
